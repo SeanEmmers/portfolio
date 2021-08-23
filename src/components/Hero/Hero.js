1eeb537625f907f990ef5ea018ac56b1dbe4b7da
Sean Phillips <seanphillips9696@gmail.com>
2021-08-23 11:14:55 +0100

    projects

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,18 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br/>
+        Sean Phillips Portfolio
+      </SectionTitle>
+      <SectionText>
+        In here write a short bio, one or two sentences...
+      </SectionText>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
